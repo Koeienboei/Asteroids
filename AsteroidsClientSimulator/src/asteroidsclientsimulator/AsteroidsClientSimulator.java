ccca2e530877fa052a9564ffc8d614916a2a31cd
Tom Eijkelenkamp <tomeijkelenkamp@hotmail.com>
2018-11-08 16:32:30 +0100

    added src all files of client simulator program

--- AsteroidsClientSimulator/src/asteroidsclientsimulator/AsteroidsClientSimulator.java
+++ AsteroidsClientSimulator/src/asteroidsclientsimulator/AsteroidsClientSimulator.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package asteroidsclient;
+package asteroidsclientsimulator;
 
 import controller.MainFrame;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 
@@ -17,23 +18,12 @@ import java.util.logging.Logger;
  *
  * @author Tom
  */
-public class AsteroidsClient {
+public class AsteroidsClientSimulator { 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        PrintStream out;
-        PrintStream err;
-        try {
-            out = new PrintStream(new FileOutputStream("output.txt"));
-            err = new PrintStream(new FileOutputStream("err.txt"));
-            System.setOut(out);
-            System.setErr(err);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(AsteroidsClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
         MainFrame mainFrame = new MainFrame();
     }
     
--- AsteroidsClientSimulator/src/controller/MainFrame.java
+++ AsteroidsClientSimulator/src/controller/MainFrame.java
@@ -6,23 +6,23 @@
 package controller;
 
 import client.Client;
-import static client.ClientState.INITIALIZE;
-import static client.ClientState.PLAYING;
+import client.ClientBot;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.Iterator;
 import java.util.LinkedList;
+import static java.util.logging.Level.FINE;
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.WARNING;
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import view.StartPanel;
 
 /**
- * Deze klasse representeert het hoofdvenster van het programma. Dit venster
- * bestaat uit de menubalk en het GamePanel-object, waarin het model getekend
- * wordt. Ook zitten hier de Action-objecten in.
+ * Deze klasse representeert het hoofdvenster van het programma. Dit venster bestaat uit de menubalk en het GamePanel-object, waarin het model getekend wordt. Ook zitten hier de Action-objecten in.
  *
  * @author Wilco Wijbrandi
  */
@@ -36,11 +36,11 @@ public class MainFrame extends JFrame {
 
     private JPanel mainPanel;
     private StartPanel startPanel;
-    
+
     public MainFrame() {
         clients = new LinkedList<>();
         mainFrame = this;
-        
+
         initActions();
         initGUI();
     }
@@ -53,24 +53,49 @@ public class MainFrame extends JFrame {
 
         mainPanel = new JPanel(new CardLayout());
         getContentPane().add(mainPanel);
-        
+
         startPanel = new StartPanel(this);
         mainPanel.add(startPanel, "start");
-        
+
         this.setVisible(true);
         this.addWindowListener(new Exit());
+    }
+
+    private synchronized void spawnClients(int amount) {
+        for (int i = 0; i < amount; i++) {
+            spawnClient();
+        }
+    }
+
+    private synchronized void spawnClient() {
+        Client client = new ClientBot(startPanel.getOperatorAddress());
+        clients.add(client);
+        client.start();
+        try {
+            this.wait(100);
+        } catch (InterruptedException ex) {
+        }
+    }
+
+    private synchronized void closeClients() {
+        Iterator<Client> it = clients.iterator();
+        while (it.hasNext()) {
+            Client client = it.next();
+            client.logger.log(FINE, "[MainFrame] Window closing");
+            client.close();
+            it.remove();
+            try {
+                this.wait(100);
+            } catch (InterruptedException ex) {
+            }
+        }
     }
 
     private void initActions() {
         this.joinAction = new AbstractAction("Join") {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                System.out.println("Spawning #" + startPanel.getAmount() + " clients");
-                for (int i=0; i<startPanel.getAmount(); i++) {
-                    Client client = new Client(startPanel.getOperatorAddress());
-                    clients.add(client);
-                    client.start();
-                }
+                spawnClients(startPanel.getAmount());
             }
         };
     }
@@ -78,7 +103,7 @@ public class MainFrame extends JFrame {
     public AbstractAction getJoinAction() {
         return joinAction;
     }
-    
+
     private class Exit implements WindowListener {
 
         @Override
@@ -86,14 +111,8 @@ public class MainFrame extends JFrame {
         }
 
         @Override
-        public void windowClosing(WindowEvent e) {
-            Iterator<Client> it = clients.iterator();
-            while (it.hasNext()) {
-                Client client = it.next();
-                if (client != null && (client.getClientState() == INITIALIZE || client.getClientState() == PLAYING)) {
-                    client.logout();
-                }
-            }
+        public synchronized void windowClosing(WindowEvent e) {
+            closeClients();
             System.exit(0);
         }
 
@@ -116,7 +135,7 @@ public class MainFrame extends JFrame {
         @Override
         public void windowDeactivated(WindowEvent e) {
         }
-        
+
     }
-    
+
 }
